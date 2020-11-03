1a04ee9ea3c150eb5d1ff3a6a81f4140b3d6de3c
HrantHaruthiunian <you@example.com>
2020-11-03 20:58:28 +0400

    commit

--- JS/JS_Homeworks/sumRecursive.js
+++ JS/JS_Homeworks/sumRecursive.js
@@ -1,15 +1,13 @@
 function sumRecursive() {
 
-   let number = 5;
+   let number = 6;
    let sum = 0;
 
-   function recursion(number) {
+   let sumRec = (function recursion(number) {
       sum = sum + number;
       if (number == 0) { return sum }
       return recursion(number - 1);
-   }
-
-   let sumRec = recursion(number);
+   })(number);
 
    return function () {
       let result = "<h3>The number is: " + number + "<br>and the sum of number's elements is: " + sumRec + "</h3><hr>" + '<i>For mor information please see the file <b>"sumRecursive.js"</b></i>';
