6376be8d271122638556491f3b4a820c458d0883
Hrant Harutyunyan <hharuthiunian@gmail.com>
2021-01-11 16:31:21 +0400

    worm game

--- JS/JS_Homeworks/gameWorm.js
+++ JS/JS_Homeworks/gameWorm.js
@@ -1,160 +1,166 @@
-let leftOffset = 10;
-let topOffset = 175;
-let step = 5;
-let temp = 100;
-let moveDirection = "";
-let moveIntervalId = "";
-let bustCount = 0;
-let gamer = "";
 
+function Worm() {
+   this.leftOffset = 10;
+   this.topOffset = 175;
+   this.step = 5;
+   this.temp = 100;
+   this.moveDirection = "";
+   this.moveIntervalId = "";
+   this.bustCount = 0;
+   this.gamer = prompt("Ինչ է քո անունը ?", "");
 
-function setWormName() {
-   gamer = prompt("Ինչ է քո անունը ?");
-   viewWorm();
+   this.presentParameters = function () {
+      document.getElementById("p2").innerHTML = `,  <b>Գլխավոր ճիճու՝</b> ${this.gamer}`;
+      document.getElementById("p3").innerHTML = `,  <b>Ճիճուի արագությունը՝</b> ${Math.round((1000 / this.temp), 0)}`;
+      document.getElementById("p4").innerHTML = `,  <b>Արագության մակարդակը՝</b> ${this.bustCount}`;
+   };
+
+   this.resetWormCoordinates = function () {
+      $("#worm").offset({
+         left: this.leftOffset,
+         top: this.topOffset
+      });
+   };
+
+   this.pushUp = function () {
+      this.topOffset -= this.step;
+
+   };
+   this.pushDown = function () {
+      this.topOffset += this.step;
+
+   };
+   this.pushLeft = function () {
+      this.leftOffset -= this.step;
+
+   };
+   this.pushRight = function () {
+      this.leftOffset += this.step;
+
+   };
+
+   this.move = function () {
+      clearInterval(this.moveIntervalId);
+      this.moveIntervalId = setInterval(this.moveDirection, this.temp);
+   };
+
+   this.stopMoving = function () {
+      clearInterval(this.moveIntervalId);
+   };
+   this.movingUp = function () {
+      this.moveDirection = this.pushUp;
+      this.move();
+   };
+
+   this.movingDown = function () {
+      this.moveDirection = this.pushDown;
+      this.move();
+   };
+
+   this.movingLeft = function () {
+      this.moveDirection = this.pushLeft;
+      this.move();
+   };
+
+   this.movingRight = function () {
+      this.moveDirection = this.pushRight;
+      this.move();
+   };
+
+   this.accelerate = function () {
+      this.bustCount++;
+      if (this.temp > 5) {
+         this.temp -= this.step;
+         this.presentParameters();
+         this.move();
+
+      } else if (this.temp <= 5) {
+         clearInterval(this.moveIntervalId);
+         alert(`Ապրես ${this.gamer}, դու հաղթեցիր: Գնա միրգ կեր:`);
+         this.temp = 100;
+         this.presentParameters();
+         this.move();
+
+      }
+   };
+
+   this.decelerate = function () {
+      this.bustCount--;
+      if (this.temp <= 500) {
+         this.temp += this.step;
+         this.move();
+         this.presentParameters();
+      } else if (this.temp > 500) {
+         clearInterval(this.moveIntervalId);
+         alert("Անչափ դանդաղ է: Պետք է արագացնեք: !!!");
+         this.temp = 500;
+         this.move();
+         this.presentParameters();
+      }
+   };
 }
 
 
-function viewWorm() {
 
-   document.getElementById("p2").innerHTML = `,  <b>Գլխավոր ճիճու՝</b> ${gamer}`;
-   document.getElementById("p3").innerHTML = `,  <b>Ճիճուի արագությունը՝</b> ${Math.round((1000 / temp), 0)}`;
-   document.getElementById("p4").innerHTML = `,  <b>Արագության մակարդակը՝</b> ${bustCount}`;
+let worm1 = new Worm();
+worm1.presentParameters();
+worm1.movingUp();
+worm1.movingDown();
+worm1.movingLeft();
+worm1.movingRight();
+worm1.move();
+controlling(worm1);
+
+
+
+
+function controlling(object) {
+
+   $("#worm").click(object.accelerate);
+
+   window.addEventListener("keydown", function (event) {
+      if (event.defaultPrevented) {
+         return; // Do nothing if the event was already processed
+      }
+
+      switch (event.key) {
+         case "ArrowDown":
+            object.movingDown();
+            document.getElementById("worm").style.transform = "rotate(90deg)";
+            break;
+         case "ArrowUp":
+            object.movingUp();
+            document.getElementById("worm").style.transform = "rotate(-90deg)";
+            break;
+         case "ArrowLeft":
+            object.movingLeft();
+            document.getElementById("worm").style.transform = "rotate(-180deg)";
+            break;
+         case "ArrowRight":
+            object.movingRight();
+            document.getElementById("worm").style.transform = "rotate(0deg)";
+            break;
+         case "Enter":
+            object.move();
+            break;
+         case "Escape":
+            object.stopMoving();
+            break;
+         case " ":
+            object.stopMoving();
+            break;
+         case "+":
+            object.accelerate();
+            break;
+         case "-":
+            object.decelerate();
+            break;
+
+         default:
+            return;
+      }
+
+      // Cancel the default action to avoid it being handled twice
+      event.preventDefault();
+   }, true);
 }
-
-function resetWormCoordinates() {
-   $("#worm").offset({
-      left: leftOffset,
-      top: topOffset
-   });
-}
-
-function moveUp() {
-   topOffset -= step;
-   resetWormCoordinates();
-}
-
-function moveDown() {
-   topOffset += step;
-   resetWormCoordinates();
-}
-
-function moveLeft() {
-   leftOffset -= step;
-   resetWormCoordinates();
-}
-
-function moveRight() {
-   leftOffset += step;
-   resetWormCoordinates();
-}
-
-function move() {
-   clearInterval(moveIntervalId);
-   moveIntervalId = setInterval(moveDirection, temp);
-}
-
-function stopMoving() {
-   clearInterval(moveIntervalId);
-}
-
-function pushUp() {
-   moveDirection = moveUp;
-   move();
-}
-
-function pushDown() {
-   moveDirection = moveDown;
-   move();
-}
-
-function pushLeft() {
-   moveDirection = moveLeft;
-   move();
-}
-
-function pushRight() {
-   moveDirection = moveRight;
-   move();
-}
-
-
-function accelerate() {
-   bustCount++;
-   if (temp > 5) {
-      temp -= step;
-      move();
-      viewWorm();
-   } else if (temp <= 5) {
-      clearInterval(moveIntervalId);
-      alert(`Ապրես ${gamer}, դու հաղթեցիր: Գնա ${null} կեր:`);
-      temp = 100;
-      move();
-      viewWorm();
-   }
-}
-
-function decelerate() {
-   bustCount--;
-   if (temp <= 500) {
-      temp += step;
-      move();
-      viewWorm();
-   } else if (temp > 500) {
-      clearInterval(moveIntervalId);
-      alert("Անչափ դանդաղ է: Պետք է արագացնեք: !!!");
-      temp = 500;
-      move();
-      viewWorm();
-   }
-}
-
-$("#worm").click(accelerate);
-
-
-
-
-window.addEventListener("keydown", function (event) {
-   if (event.defaultPrevented) {
-      return; // Do nothing if the event was already processed
-   }
-
-   switch (event.key) {
-      case "ArrowDown":
-         pushDown();
-         document.getElementById("worm").style.transform = "rotate(90deg)";
-         break;
-      case "ArrowUp":
-         pushUp();
-         document.getElementById("worm").style.transform = "rotate(-90deg)";
-         break;
-      case "ArrowLeft":
-         pushLeft();
-         document.getElementById("worm").style.transform = "rotate(-180deg)";
-         break;
-      case "ArrowRight":
-         pushRight();
-         document.getElementById("worm").style.transform = "rotate(0deg)";
-         break;
-      case "Enter":
-         move();
-         break;
-      case "Escape":
-         stopMoving();
-         break;
-      case " ":
-         stopMoving();
-         break;
-      case "+":
-         accelerate();
-         break;
-      case "-":
-         decelerate();
-         break;
-
-      default:
-         return;
-   }
-
-   // Cancel the default action to avoid it being handled twice
-   event.preventDefault();
-}, true);
